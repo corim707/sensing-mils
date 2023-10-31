ae627c733f5e0adb7e33f928595b5d2e8692136e
corim707 <corim707@hotmail.com>
2023-10-30 20:38:56 -0700

    Consume dependencies and add appropriate field calls

--- index.mjs
+++ index.mjs
@@ -1,13 +1,16 @@
-import 'axios'      // Used to make HTTP Requests
-import moment from 'momentjs'   // Used for time
+import axios from 'axios'      // Used to make HTTP Requests
+import moment from 'moment'   // Used for time
 import {appendFileSync} from 'fs';
 
 const FILE_NAME_TO_OUTPUT = `sensing-mills-sensors.csv`
 const API_KEY = ``;
 const SENSOR_IDS = [
-    `sensor1`,
+    'sensor1'
 ]
 
+const FIELDS = [
+    `humidity`, `humidity_a`, `humidity_b`, `temperature`, `temperature_a`, `temperature_b`, `pressure`, `pressure_a`, `pressure_b`
+]
 
 /**
  * Configure the Axios request library (https://www.npmjs.com/package/axios#interceptors) to
@@ -34,9 +37,9 @@ const setupAxios = (apiKey) => {
  */
 const getSensorData = async ({sensorId, startTime, endTime})=>{
     const requestUrl = `https://api.purpleair.com/v1/sensors/${sensorId}/history/csv`;
-    const params = {};
+    const params = { fields: FIELDS.join(',')};
     if (startTime) params[`start_timestamp`] = startTime;
-    if (endTime) params[`end_timestamp`] = endTime;
+    if (endTime) params[`end_timestamp`] = endTime.format('x');
     return axios.get(requestUrl, params);
 }
 
@@ -46,9 +49,9 @@ const endTime = moment().subtract(1, 'day');
 
 let sensorDataCollection = [];
 
-for (const sensor in SENSOR_IDS){
+for (const sensorId in SENSOR_IDS){
     const sensorData = await getSensorData( {sensorId, endTime});
-    sensorDataCollection.push( sensorData );
+    sensorDataCollection.push( sensorData.data );
 }
 
 const fullCsv = sensorDataCollection.join(`\n`); // Take the CSV data from each sensor and concatenate it into a single file
